355988a1cd6a105384a1dc2b1ec4c93b5877cc8a
Eli Atkinson <atkinson.eli@gmail.com>
2020-05-14 01:27:04 -0400

    added bales per hour functionality

--- index.js
+++ index.js
@@ -1,53 +1,79 @@
+let HOST = "http://localhost:3000"
+
+
 window.onload = () => {
 
     let baleCount = 0;
-    let bales_data;
+    let response;
 
     //SETUP xhr request
     var xhr = new XMLHttpRequest();
+    var xhr2 = new XMLHttpRequest();
+
+    xhr2.onload = function() {
+
+        let response;
+
+        if (xhr2.status >= 200 && xhr2.status < 300) {
+            // This will run when the request is successful
+            
+            response = JSON.parse(xhr2.response);
+        } else {
+            // This will run when it's not
+            console.log('The request failed!');
+        }
+
+        let gin = document.getElementById("gin");
+        gin.innerText = response.gin;
+
+        console.log('Get response',response)
+
+
+    }
 
     xhr.onload = function () {
 
         // Process our return data
         if (xhr.status >= 200 && xhr.status < 300) {
             // This will run when the request is successful
-            console.log('success!', xhr);
-            bales_data = JSON.parse(xhr.response);
+            
+            response = JSON.parse(xhr.response);
         } else {
             // This will run when it's not
             console.log('The request failed!');
         }
     
-
+        let bales_list = response.bales.list;     //list is an array of bales
         let d = new Date()
         let cutoff = d.getTime() - (1000 * 60 * 60);  //to get bales from last hour
         
-        showBales(bales_data, 0)            //note bales cutoff being implemnted server side for now
+        showBales(bales_list, 0)            //note bales cutoff being implemnted server side for now
+        showAverage(bales_list)
        
         var current = document.getElementById("#current");
     
         
     };
 
-   
+   xhr2.open('GET', `${HOST}/gin`)
+   xhr2.send();
 
     setInterval(()=> {
         console.log("bales read");
         baleCount++;
 
-        xhr.open('GET', 'http://10.1.10.78:3000/latest');
+        //xhr.open('GET', 'http://10.1.10.78:3000/latest');
+        xhr.open('GET', `${HOST}/latest`);
+
         xhr.send();
 
 
     }, 10000);
 }
 
-function showBales(bales_data, cutoff){
+function showBales(bales_list, cutoff){
 
     var viewer = document.getElementById("currentdisplay");
-
-
-    console.log("the type of bales_data IS...", typeof bales_data)
    
 
     let bales = []
@@ -67,11 +93,17 @@ function showBales(bales_data, cutoff){
     // }
 
     //make an array of ONLY bales that are not alread in the DOM
-    for(key in bales_data){
-        if(!shownTags.includes(bales_data[key].tag)){
-            bales.push(bales_data[key]);
+    // for(key in bales_data){
+    //     if(!shownTags.includes(bales_data[key].tag)){
+    //         bales.push(bales_data[key]);
+    //     }
+    // }
+
+    bales_list.forEach(bale => {
+        if(!shownTags.includes(bale.tag)){
+            bales.push(bale)
         }
-    }
+    })
 
     console.log("After filter bales to show of ", bales.length);
 
@@ -98,6 +130,46 @@ function showBales(bales_data, cutoff){
 
     }
     
+
+}
+
+function showAverage(bales_list){
+
+    let balesPerHour = document.getElementById("bph");
+    let minPerBale = document.getElementById("pb");
+
+    if(bales_list.length < 2){
+        return;
+    }
+
+    let firstBaleTime = Infinity;
+    let lastBaleTime = -Infinity;
+
+    
+    bales_list.forEach(bale => {
+
+        console.log("Bale time ",bale.time)
+        if(bale.time < firstBaleTime) firstBaleTime = bale.time
+        if(bale.time > lastBaleTime) lastBaleTime = bale.time
+
+    })
+        
+    
+        
+
+    
+
+    let baleTimeSpan = lastBaleTime - firstBaleTime;
+
+    console.log("First bale and last bale at for a diff of", firstBaleTime, lastBaleTime, baleTimeSpan)
+
+    let avgTimeMin = Math.floor((baleTimeSpan/bales_list.length)/(1000*60))
+    let avgTimeSec = Math.floor(((baleTimeSpan/bales_list.length)/1000)%60).toString().padStart(2,'0');
+
+    let avgTime = `${avgTimeMin}:${avgTimeSec}`
+
+    minPerBale.innerText = avgTime;
+    balesPerHour.innerText = (60/((baleTimeSpan/bales_list.length)/(1000*60))).toPrecision(3);
 
 
 }
